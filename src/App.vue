6c0678d6c206d5584dfa78b83c5c8c6fe612ddf7
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-31 12:53:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import Header from './components/Header.vue'
+  import TaskTable from './components/task-table.vue'
+</script>
+
+<template>
+  <div>
+    <Header />
+    <TaskTable />
+  </div>
+</template>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import Button from './ui/button/Button.vue'
+</script>
+
+<template>
+  <header class="flex justify-between px-6 py-2">
+    <a href="/" class="font-bold text-2xl italic">Tasklify</a>
+    <Button variant="link">Create task</Button>
+  </header>
+</template>
